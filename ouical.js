5e5587cddc2bc2178d9aec551b5881fb7fe9035f
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-06-06 23:09:34 -0400

    Make validParams() fn to check params.  Make it check explicitly for undefined.

--- ouical.js
+++ ouical.js
@@ -103,6 +103,12 @@
     };
   };
 
+  // Make sure we have the necessary event data, such as start time and event duration
+  var validParams = function(params) {
+    return params.data !== undefined && params.data.start !== undefined &&
+      (params.data.end !== undefined || params.data.duration !== undefined);
+  };
+
   exports.createCalendar = function(params) {
     var result = document.createElement('div');
     var calendarId = (params.options && params.options.id) ?
