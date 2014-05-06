d900c41284b2af7b2e9cb91c86a9814207e379cc
carl sednaoui <carlsednaoui@gmail.com>
2014-05-05 23:14:01 -0400

    Continue working on the build process. moment is clashing with UMD wrapper

--- build/ouical.js
+++ build/ouical.js
@@ -1,4 +1,15 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+
+(function(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(["moment"], factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory(require('moment'));
+  } else {
+    root.Ouical = factory(root.moment);
+  }
+}(this, function(moment) {
+
+return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (global){
 //! moment.js
 //! version : 2.6.0
@@ -2545,4 +2556,6 @@ Ouical = (function() {
 module.exports = Ouical;
 
 
-},{"moment":1}]},{},[2])
+},{"moment":1}]},{},[2]);
+
+}));
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp       = require('gulp')
 ,   path       = require('path')
 ,   watch      = require('gulp-watch')
 ,   source     = require('vinyl-source-stream')
-,   browserify = require('browserify');
+,   browserify = require('browserify')
+,   wrap       = require('gulp-wrap-umd')
+,   buffer     = require('gulp-buffer');
 
 gulp.task('default', function() {
   gulp.run('build', 'minify');
@@ -12,22 +14,34 @@ gulp.task('build', function() {
   // start browserify and compile the file from coffee
   // use bundle to return a readable stream of the compiled contents
   // this stream will be passed to gulp
+  // .pipe(source('ouical.js')) is the transition from browserify to gulp
+  // we use buffer because wrap-umd does not have streaming support
+
   browserify(path.join(__dirname, 'src/ouical.coffee'))
     .transform('coffeeify')
     .bundle()
     .pipe(source('ouical.js'))
+    .pipe(buffer())
+    .pipe(wrap({ namespace: 'Ouical',
+      deps: [
+        { name: 'moment', globalName: 'moment', paramName: 'moment' }
+      ]
+    }))
     .pipe(gulp.dest('build'));
 });
 
 gulp.task('minify', function() {
-  // start browserify and compile the file from coffee
-  // use bundle to return a readable stream of the compiled contents
-  // this stream will be passed to gulp
   browserify(path.join(__dirname, 'src/ouical.coffee'))
     .transform('coffeeify')
     .transform({ global: true }, 'uglifyify')
     .bundle()
     .pipe(source('ouical.min.js'))
+    .pipe(buffer())
+    .pipe(wrap({ namespace: 'Ouical',
+      deps: [
+        { name: 'moment', globalName: 'moment', paramName: 'moment' }
+      ]
+    }))
     .pipe(gulp.dest('build'));
 });
 
