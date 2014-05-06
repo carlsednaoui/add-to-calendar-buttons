68551b05d0332182087673457747bf4e0fe47937
carl sednaoui <carlsednaoui@gmail.com>
2014-05-05 22:49:05 -0400

    Implement browserify to build the client side JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,33 @@
-var gulp   = require('gulp')
-,   coffee = require('gulp-coffee')
-,    watch = require('gulp-watch');
+var gulp       = require('gulp')
+,   path       = require('path')
+,   watch      = require('gulp-watch')
+,   source     = require('vinyl-source-stream')
+,   browserify = require('browserify');
 
 gulp.task('default', function() {
-  gulp.src('src/ouical.coffee')
-    .pipe(coffee({bare: true}))
+  gulp.run('build', 'minify');
+});
+
+gulp.task('build', function() {
+  // start browserify and compile the file from coffee
+  // use bundle to return a readable stream of the compiled contents
+  // this stream will be passed to gulp
+  browserify(path.join(__dirname, 'src/ouical.coffee'))
+    .transform('coffeeify')
+    .bundle()
+    .pipe(source('ouical.js'))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('minify', function() {
+  // start browserify and compile the file from coffee
+  // use bundle to return a readable stream of the compiled contents
+  // this stream will be passed to gulp
+  browserify(path.join(__dirname, 'src/ouical.coffee'))
+    .transform('coffeeify')
+    .transform({ global: true }, 'uglifyify')
+    .bundle()
+    .pipe(source('ouical.min.js'))
     .pipe(gulp.dest('build'));
 });
 
